7f5288ee97cbccd9bf2ac1c6c3a536047b0b29b6
Eric Tuchscherer <eric@tuchscherer.me>
2019-11-28 12:14:14 -0800

    Stubbing out the landing page

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,61 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Gro-box</v-toolbar-title>
       <v-app-bar-nav-icon>
         <v-icon>
           mdi-leaf
         </v-icon>
       </v-app-bar-nav-icon>
-      <v-toolbar-title>Gro-box</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn text>
+        <span class="mr-2">Settings</span>
+        <v-icon>mdi-settings-outline</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <v-container class="grey lighten-5">
+        <v-row>
+          <v-col>
+            <v-card flat class="pa-4">
+              System Status
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card class="pa-4">
+              <v-card-title>
+                Monitor
+                <v-icon large class="ma-2">mdi-tablet-dashboard</v-icon>
+              </v-card-title>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="pa-4">
+              <v-card-title>
+                Control
+                <v-icon large class="ma-2">mdi-tools</v-icon>
+              </v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+// import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    // HelloWorld
   },
 
   data: () => ({
